279ed9058b50023794225531534844a530f65962
Kalcho815 <kaloynwa@gmail.com>
2019-08-14 13:29:56 +0300

    Deleted unused code

--- KWin/Seeding/KWinMatchSeeder.cs
+++ KWin/Seeding/KWinMatchSeeder.cs
@@ -20,14 +20,9 @@ namespace KWin.Seeding
         {
             if (context.Matches.Count() < 5)
             {
-
-
                 Team[] teams = context.Teams.ToArray();
-                Random random = new Random();
 
-                List<MatchTeam> participatingTeams = new List<MatchTeam>();
                 List<Match> matchesToAdd = new List<Match>();
-                //TODO: Fix
                 for (int i = 0; i < 5 - context.Matches.Count(); i++)
                 {
 
@@ -38,20 +33,14 @@ namespace KWin.Seeding
                         League = teams[i].League,
                     };
 
-                    //match.MatchTeams = participatingTeams;
-
                     matchesToAdd.Add(match);
                 }
 
                 context.Matches.AddRange(matchesToAdd);
                 context.SaveChanges();
 
-                //context.Teams.ToList().Select(t => t.MatchTeams = new MatchTeam
-                //{
-                //    TeamId = t.Id,
-                //    MatchId = matchesToAdd.ToArray()[randomMatch.Next(0, 5)].Id
-                //});
             }
+
             MappingTeamsToMatches();
 
         }
@@ -82,45 +71,8 @@ namespace KWin.Seeding
                     }
                     break;
                 }
-
             }
         }
 
-        private List<Team> GetTwoRandomTeams(Random random, Team[] teams, List<MatchTeam> participatingTeams)
-        {
-
-            int firstTeam = 0;
-            List<Team> teamsToReturn = new List<Team>();
-            while (true)
-            {
-                firstTeam = random.Next(0, 10);
-                Team firstTeamToReturn = teams[firstTeam];
-                if (context.MatchTeams.Select(mt => mt.TeamId).Contains(firstTeamToReturn.Id)
-                    || participatingTeams.Any(t => t.TeamId == firstTeamToReturn.Id))
-                {
-                    continue;
-                }
-                teamsToReturn.Add(firstTeamToReturn);
-                break;
-            }
-
-            while (true)
-            {
-                int secondTeam = random.Next(0, 10);
-                Team secondTeamToReturn = teams[secondTeam];
-
-
-                if (context.MatchTeams.Select(mt => mt.TeamId).Contains(secondTeamToReturn.Id)
-                    || secondTeamToReturn.Id == teams[firstTeam].Id
-                    || participatingTeams.Any(t => t.TeamId == secondTeamToReturn.Id))
-                {
-                    continue;
-                }
-                teamsToReturn.Add(secondTeamToReturn);
-                break;
-            }
-
-            return teamsToReturn;
-        }
     }
 }
