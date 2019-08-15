126f254f671bc7e8ca4bb23be3fbf50d8f2d8408
Kalcho815 <kaloynwa@gmail.com>
2019-08-15 15:01:23 +0300

    Added AllMatches view and AllMatches method in the MatchesController

---
+++ KWin/Controllers/MatchesController.cs
@@ -0,0 +1,22 @@
+﻿using KWin.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KWin.Controllers
+{
+    public class MatchesController : Controller
+    {
+        public MatchesController(IMatchesService matchesService)
+        {
+
+        }
+
+        public IActionResult AllMatches()
+        {
+            return this.View();
+        }
+    }
+}
