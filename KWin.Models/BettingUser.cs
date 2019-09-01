9cc8b46196ff1291868d0a7814a7413981a13b70
Kalcho815 <kaloynwa@gmail.com>
2019-09-01 16:04:24 +0300

    MatchTeam.cs and BettingUser.cs

---
+++ KWin.Models/BettingUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace KWin.Models
+{
+    public class BettingUser : IdentityUser
+    {
+        public BettingUser()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Bets = new HashSet<Bet>();
+        }
+
+        [Required]
+        public decimal Balance { get; set; }
+
+        public ICollection<Bet> Bets { get; set; }
+    }
+}
---
+++ KWin.Models/MatchTeam.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KWin.Models
+{
+    public class MatchTeam
+    {
+        [Required]
+        public string MatchId { get; set; }
+        public Match Match { get; set; }
+
+        [Required]
+        public string TeamId { get; set; }
+        public Team Team { get; set; }
+    }
+}
